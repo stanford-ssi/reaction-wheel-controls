2cd8e8fcf971f0c5a50c80930a6af24b5c57e17b
Roman Dimov <romanddimov@gmail.com>
2019-01-17 17:49:44 -0800

    remove libraries from git dir

--- ArduinoControl/ReactionWheelController/ReactionWheelController.ino
+++ ArduinoControl/ReactionWheelController/ReactionWheelController.ino
@@ -1,8 +1,8 @@
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
-#include "I2Cdev.h"
-#include "MPU6050.h"
-#include "helper_3dmath.h"
+#include <I2Cdev/I2Cdev.h>
+#include <MPU6050.h>
+#include <helper_3dmath.h>
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
